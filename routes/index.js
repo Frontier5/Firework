118c8a1751f8a0bc81fb68276198db47ff84ba0d
Abhishek Kasireddy <akasireddy99@gmail.com>
2019-10-11 07:23:04 +0530

    Wowza

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+// Auth protected route
+
+// If no token, throw back, if token -> check role for student, if not student, throw back
+
+router.post('/student', function (req, res, next) {
+	
+});
+
+module.exports = router;
